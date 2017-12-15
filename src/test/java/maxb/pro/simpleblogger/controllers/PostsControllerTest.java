94962f32cce014831bc7abdbc7246181e1f4c5b1
MaxB <maxbpro2009@gmail.com>
2017-12-15 19:30:58 +0300

    Updated tests fpr post controller

--- src/test/java/maxb/pro/simpleblogger/controllers/PostsControllerTest.java
+++ src/test/java/maxb/pro/simpleblogger/controllers/PostsControllerTest.java
@@ -3,14 +3,25 @@ package maxb.pro.simpleblogger.controllers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(PostsController.class)
 public class PostsControllerTest {
+
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private PostsController controller;
+
     @Before
     public void setUp() throws Exception {
     }
