df1ded53fb30e062fdc0cee4d096ddc602aae57f
MaxB <maxbpro2009@gmail.com>
2018-01-13 19:23:14 +0300

    Updated properties

--- src/main/java/maxb/pro/simpleblogger/controllers/PostsController.java
+++ src/main/java/maxb/pro/simpleblogger/controllers/PostsController.java
@@ -27,18 +27,18 @@ public class PostsController {
     @PostConstruct
     private void initPosts(){
 
-        postsRepository.deleteAll();
-
-        for(int i = 0; i < 10; i++){
-            Post post = new Post();
-            post.setTitle("post" + i);
-            post.setTags("tag 1, tg 2");
-            post.setKeywords("keyword1, keyword2");
-            post.setContent("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum ");
-            post.setAuthor("Maxb");
-            post.setPermalink("permalink1");
-            postsService.savePost(post);
-        }
+//        postsRepository.deleteAll();
+//
+//        for(int i = 0; i < 10; i++){
+//            Post post = new Post();
+//            post.setTitle("post" + i);
+//            post.setTags("tag 1, tg 2");
+//            post.setKeywords("keyword1, keyword2");
+//            post.setContent("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum ");
+//            post.setAuthor("Maxb");
+//            post.setPermalink("permalink1");
+//            postsService.savePost(post);
+//        }
 
 
     }
